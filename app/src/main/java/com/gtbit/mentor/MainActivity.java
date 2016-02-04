bd4508fb382d5a0870fb20093dc9e3fe798a8277
jasbir1612 <jasbir1612@gmail.com>
2016-02-04 11:02:41 +0530

    viewpager

--- app/src/main/java/com/gtbit/mentor/MainActivity.java
+++ app/src/main/java/com/gtbit/mentor/MainActivity.java
@@ -1,13 +1,25 @@
-hackhackapackage com.gtbit.mentor;
 
-import android.support.v7.app.AppCompatActivity;
+package com.gtbit.mentor;
+
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager;
+        ViewPagerAdapter adapter;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+//            viewPager = (ViewPager) findViewById(R.id.view_pager);
+//            adapter = new ViewPagerAdapter(this);
+//            viewPager.setAdapter(adapter);
+
+
+        }
     }
-}
